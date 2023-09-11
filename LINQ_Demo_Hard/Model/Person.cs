3c5a1992e0fd199a1178fe116238afa9a56a2908
Starinin Andrey <starininandrey@gmail.com>
2023-09-11 19:23:00 +0300

    Initial commit

---
+++ LINQ_Demo_Hard/Model/Person.cs
@@ -0,0 +1,54 @@
+﻿namespace LINQ_Demo_Hard.Model;
+
+//public record Person(Guid Guid, string FirstName, string LastName, DateTime DateOfBirth);
+
+public record class Person
+{
+    public Guid Guid { get; init; }
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public DateTime DateOfBirth { get; init; }
+}
+
+/*public class Person : IEquatable<Person>
+{
+    public Guid Guid { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.Guid}: {this.LastName} {this.FirstName}, {this.DateOfBirth:d}";
+    }
+
+    public bool Equals(Person? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Guid.Equals(other.Guid) && FirstName == other.FirstName && LastName == other.LastName && DateOfBirth.Equals(other.DateOfBirth);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Person)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Guid, FirstName, LastName, DateOfBirth);
+    }
+
+    public static bool operator ==(Person? left, Person? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Person? left, Person? right)
+    {
+        return !Equals(left, right);
+    }
+}*/
---
+++ LINQ_Demo_Hard/Program.cs
@@ -0,0 +1,31 @@
+﻿using LINQ_Demo_Hard.Model;
+
+var id = Guid.NewGuid();
+
+var person1 = new Person
+{
+    Guid = id,
+    FirstName = "Andrey",
+    LastName = "Starinin",
+    DateOfBirth = new DateTime(1986, 2, 18)
+};
+
+var person2 = new Person
+{
+    Guid = id,
+    FirstName = "Andrey",
+    LastName = "Starinin",
+    DateOfBirth = new DateTime(1986, 2, 18)
+};
+
+//Console.WriteLine($"{person1.Guid}: {person1.LastName} {person1.FirstName}, {person1.DateOfBirth:d}");
+//Console.WriteLine($"{person2.Guid}: {person2.LastName} {person2.FirstName}, {person2.DateOfBirth:d}");
+
+Console.WriteLine(person1);
+Console.WriteLine(person2);
+
+Console.WriteLine(person1.GetHashCode());
+Console.WriteLine(person2.GetHashCode());
+Console.WriteLine(person1 == person2);
+
+Console.WriteLine(person1.Equals(person2));
